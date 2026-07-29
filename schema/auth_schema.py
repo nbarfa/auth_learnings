931da51c35a39a423fb494e543a4b0ec3a905f3e
Nitin Barfa <nitinbarfa52@gmail.com>
2026-07-29 18:53:26 +0530

    create the registerschema for validation

--- schema/auth_schema.py
+++ schema/auth_schema.py
@@ -3,10 +3,19 @@ from marshmallow import Schema, fields, validate
 class RegisterSchema(Schema):
     username = fields.Str(
         required=True,
-        validate=validate.Length(min=3, max=20)
+        validate=[
+            validate.Length(min=3, max=20),
+            validate.Regexp(r"^[A-Za-z0-9_]+$")
+        ]
         )
 
-
     email = fields.Email(required=True)
-    password = fields.Str(required=True)
+
+    password = fields.Str(
+        required=True,
+        validate=[
+            validate.Length(min=8),
+            validate.Regexp(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$")
+        ]
+    )
 
